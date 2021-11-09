fcec4bb884fd49a7554b0243341b687d852b0434
Eleanor Bo <yb2503@columbia.edu>
2021-11-09 12:52:28 -0500

    Update test_cipher_yb2503.py

--- cipher_yb2503/tests/test_cipher_yb2503.py
+++ cipher_yb2503/tests/test_cipher_yb2503.py
@@ -1,8 +1,8 @@
 from cipher_yb2503 import cipher_yb2503
-import pytest 
+import pytest
 
 def test_cipher_single():
     example = 'test'
     expected = 'vguv'
-    actual = cipher_yb2403.cipher(example, 2, True)
+    actual = cipher_yb2503.cipher(example, 2, True)
     assert actual == expected, 'not works for a single word'
