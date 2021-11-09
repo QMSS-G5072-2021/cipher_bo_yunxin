bae6c28c49c448d3844584a747eb07f4938bdcec
Eleanor Bo <yb2503@columbia.edu>
2021-11-09 10:44:56 -0500

    first commit

---
+++ cipher_yb2503/tests/test_cipher_yb2503.py
@@ -0,0 +1,8 @@
+from cipher_yb2503 import cipher_yb2503
+import pytest 
+
+def test_cipher_single():
+    example = 'test'
+    expected = 'vguv'
+    actual = cipher_ry2403.cipher(example,2,True)
+    assert actual == expected,'not works for a single word'
